1e5472285685839e8fe99525ded1ebbce5f6784e
Manuel Tomis <manuel@codegremlins.com>
2011-01-01 01:57:12 +0200

    print javascript

---
+++ src/com/codegremlins/lemonjuice/engine/JavascriptTemplateVisitor.java
@@ -0,0 +1,110 @@
+package com.codegremlins.lemonjuice.engine;
+
+public class JavascriptTemplateVisitor implements TemplateElementVisitor {
+    private StringBuilder out = new StringBuilder();
+    
+    public Object visit(AndElement element) throws Exception {
+        // out.print("(");
+        //out.print("_true("
+        // item.visit(this);
+        //out.print(")"
+        // out.print(")");
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(ApplyElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(ConstantElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(DefineElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(ForElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(ForInlineElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(FunctionElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(IfElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(IncludeElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(IndirectPropertyElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(InlineElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(ListElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(MapElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(NotElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(OrElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(PropertyElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(SequenceElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(SetElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(TextElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object visit(ValueElement element) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
